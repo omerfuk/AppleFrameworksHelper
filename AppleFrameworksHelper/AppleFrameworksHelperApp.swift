3cd8fafd74586e09134de6183e96a9210b7a9f56
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-20 15:18:39 +0300

    Initial Commit

---
+++ AppleFrameworksHelper/AppleFrameworksHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksHelperApp.swift
+//  AppleFrameworksHelper
+//
+//  Created by Ömer Faruk Kılıçaslan on 20.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
